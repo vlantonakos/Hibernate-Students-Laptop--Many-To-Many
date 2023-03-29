123ea2f343fe9927215ff8a238023a7c8f30141e
Antonis <107072477+vlantonakos@users.noreply.github.com>
2023-03-29 12:06:25 +0300

    Add files via upload

---
+++ DemoHibe/src/main/java/com/vlanto/DemoHibe/Laptop.java
@@ -0,0 +1,41 @@
+package com.vlanto.DemoHibe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Laptop {
+	
+	@Id
+	private int lid;
+	private String lname;
+	@ManyToMany
+	private List<Student> student = new ArrayList<Student>();
+	
+	public int getLid() {
+		return lid;
+	}
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	
+	public List<Student> getStudent() {
+		return student;
+	}
+	public void setStudent(List<Student> student) {
+		this.student = student;
+	}
+	
+	
+}
